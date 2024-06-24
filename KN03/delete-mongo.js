cc55d496be37986bb17c59430fd3ec48cd0b2468
auroragjemaj <auroraagjemaj@gmail.com>
2024-06-24 10:28:33 +0200

    fixed things

--- KN03/delete-mongo.js
+++ KN03/delete-mongo.js
@@ -1,6 +1,6 @@
 db.teams.deleteOne({"_id": ObjectId("56a7e1e8b2e7c6541f8032d9")})
-db.orders.deleteMany({"teamId": "teamLazio"})
-db.products.deleteMany({ $or: [ { _id: ObjectId("56a7e1e8b2e7c6541f8032d9") }, { _id: ObjectId("58d07a4d48b5770013f41e2f") } ] })
+db.gruppierungen.deleteMany({"teamId": "teamLazio"})
+db.trainer.deleteMany({ $or: [ { _id: ObjectId("56a7e1e8b2e7c6541f8032d9") }, { _id: ObjectId("58d07a4d48b5770013f41e2f") } ] })
 
 
 
